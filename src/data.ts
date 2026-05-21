a0030758be47f7753fbed09e5aa98b38056a2706
patadesanika <sanika.patade1510@gmail.com>
2026-05-21 20:33:54 +0530

    chaging email

--- src/data.ts
+++ src/data.ts
@@ -502,7 +502,7 @@ export const COMPANY_OFFICES: CompanyOffice[] = [
     city: 'Pune',
     country: 'India',
     phone: '+91 9730092433',
-    email: 'kewal.patade@atlaschemical.example.com',
+    email: 'kewal.patade@gmail.com',
     address: '20, B-2001, VTP Bel Air, Nande Road, Baner, Mahalunge, Pune, Maharashtra 411045',
     coords: { x: 70.2, y: 44.5 },
     contactPerson: 'Kewal Patade'
